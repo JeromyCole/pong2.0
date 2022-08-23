3e4c6c35050d98878ee1b71c410e2d4c1c5baa18
Jeromy Cole <mail@jeromycole.com>
2022-08-23 15:37:57 -0600

    New features: ball physics, startup screen, 2 new special attacks per player, update readme and more

--- README.md
+++ README.md
@@ -3,27 +3,37 @@ Pong 2.0
 WARNING: FLASHING LIGHTS
 
 Controls
-
 Player A:
-UP - 'W'
-DOWN - 'S'
+
+Up - 'W'
+Down - 'S'
+Special Attack - 'Space'
 
 Player B:
-UP - '8'
-DOWN - '2'
 
+Up - '8'
+Down - '2'
+Special Attack - '0'
 
 Ball speed:
-If ball speed is too slow/fast, adjust on lines 42 and 42 the variables ball.dx and ball.dy.
+If ball speed is too slow/fast, adjust declarations of variables ball.dx and ball.dy as needed.
 
 Number of rounds until game completion
-Lines 120 and 148 control how many rounds are needed to complete game. Currently set for 3 rounds. Output of winner displayed on screen as well as sent to console.
+Lines 146 and 176 control how many rounds are needed to complete game. Currently set for 3 rounds. Output of winner displayed on screen as well as sent to console.
 
 
 Updates Added:
--Colored paddles that when player scores turn ball into their paddle's color.
+-Super attack (Player can automatically return the ball chaotically) - Current odds of cast are 1/10
 
--Progressively increased speed upon ball hit (Recycled code someone else   came up with)
+-Super attack (Player can manually return the ball chaotically) - Slam key at exact moment ball hits paddle. Ball enlarges and becomes almost unstopable.
+
+-Start up screen with countdown
+
+-Paddle collision counter
+
+-When ball collides with paddle, ball's returning y axis is now random
+
+-Colored paddles that when player scores turn ball into their paddle's color.
 
 -Reset ball speed to starting speed after each round.
 
@@ -35,15 +45,20 @@ Updates Added:
 
 -Fixed paddle collisions where ball collides with paddle incorrectly
 
+-Progressively increased speed upon ball hit (*Recycled code someone elsecame up with)
+
 
 Updates Coming:
-
 -Paddle movements automove on key hold
 
 -Paddle speed and direction effecting ball speed and direction
 
 -Power ups: Randomly generated items that if touched with paddle will either    hurt or help player. Such as paddle size reduction/enhancement, multi paddle, multi ball that doesn't return to hurt sender.
  and more.
+
+-Random ball direction at startup
+
+-Give option for larger resolution gameplay.
 
 -Paddle size increases when score point?
 
--- pong2.0.py
+++ pong2.0.py
@@ -1,11 +1,13 @@
-# Pong
+# Pong Remixed
 
 import time
 import turtle
 import keyboard
+import random
 
+#Window specs & info
 wn = turtle.Screen()
-wn.title("Pong by Jeromy")
+wn.title("Pong Remixed")
 wn.bgcolor("black")
 wn.setup(width=800, height=600)
 wn.tracer(0)
@@ -13,6 +15,10 @@ wn.tracer(0)
 #Scores
 score_a = 0
 score_b = 0
+
+#Collision count
+a_collisions = 0
+b_collisions = 0
 
 # Paddle A
 paddle_a = turtle.Turtle()
@@ -54,38 +60,60 @@ pen.write("Player A : 0  Player B : 0", align="center", font=("courier", 24, "no
 # Paddle A functions
 def paddle_a_up():
     y = paddle_a.ycor()
-    y += 25.8
+    y += 39.8
     paddle_a.sety(y)
 
 def paddle_a_down():
     y = paddle_a.ycor()
-    y += -25.8
+    y += -39.8
     paddle_a.sety(y)
 
 # Paddle A keyboard binding while True:
 wn.listen()
-wn.onkey(paddle_a_up, "w")
-wn.onkey(paddle_a_down, "s")
+wn.onkeypress(paddle_a_up, "w")
+wn.onkeypress(paddle_a_down, "s")
 
 # Paddle B functions
 def paddle_b_up():
     y = paddle_b.ycor()
-    y += 25.8
+    y += 39.8
     paddle_b.sety(y)
 
 def paddle_b_down():
     y = paddle_b.ycor()
-    y += -25.8
+    y += -39.8
     paddle_b.sety(y)
 
 # Paddle B keyboard binding while True:
 wn.listen()
-wn.onkey(paddle_b_up, "8")
-wn.onkey(paddle_b_down, "2")
+wn.onkeypress(paddle_b_up, "Up")
+wn.onkeypress(paddle_b_down, "Down")
 
 ### Main game loop
 while True:
-        # Move the ball
+
+    #Startup screen -- Countdown and gameplay window prep
+    if ball.ycor() == 0: # <---- Probably not best but hasn't broke yet (once additional colision physics are added, could cause bug from the coordinates)
+        pen.goto(0, 0)
+        pen.clear()
+        pen.write("3", align="center", font=("lato", 95, "normal"))
+        pen.clear()
+        time.sleep(1)
+        pen.write("2", align="center", font=("lato", 95, "normal"))
+        pen.clear()
+        time.sleep(1)
+        pen.write("1", align="center", font=("lato", 95, "normal"))
+        pen.clear()
+        time.sleep(1)
+        pen.color('green')
+        pen.write("START", align="center", font=("lato", 100, "normal"))
+        pen.clear()
+        time.sleep(.4)
+        pen.color('white')
+        pen.goto(0, 260)
+        pen.write("Player A: 0  Player B: 0", align="center", font=("courier", 24, "normal"))
+
+    # Move the ball
     ball.setx(ball.xcor() + ball.dx)
     ball.sety(ball.ycor() + ball.dy)
     wn.update()
@@ -108,11 +136,13 @@ while True:
         ball.dx *= -1
         score_a += 1
         ball.color("red")
+        ball.shape("circle")
         wn.bgcolor("red")
         pen.clear()
+        ball.shapesize(stretch_wid=1, stretch_len=1)
         #Reset ball speed if Player A scores
-        ball.dx = 0.11
-        ball.dy = 0.12
+        ball.dx = 0.19
+        ball.dy = 0.16
         pen.write("Player A: {}  Player B: {}".format(score_a, score_b), align="center", font=("courier", 24, "normal"))
         time.sleep(.1)
         wn.bgcolor("black")
@@ -122,22 +152,29 @@ while True:
             wn.clear()
             wn.bgcolor("red")
             pen.goto(0, 160)
-            pen.write("Player A Wins with {} points!" .format(score_a), align="center", font=("lato", 32, "normal"))
-            print("Player A Wins with {} points!" .format(score_a))
+            pen.goto(0, 160)
+            pen.write("Player A Wins with:" .format(score_a, a_collisions), align="center", font=("lato", 32, "normal"))
+            pen.goto(0, 100)
+            pen.write("{} points" .format(score_a), align="center", font=("lato", 32, "normal"))
+            pen.goto(0, 40)
+            pen.write("You hit the ball {} time(s) " .format(a_collisions), align="center", font=("lato", 32, "normal"))
+            print("Player A Wins with {} points and {} collisions" .format(score_a, a_collisions))
             time.sleep(3)
             break
 
-       #Ball passes paddle on left - Player B scored, ball changes to their color, point added, speed adjusted
+    #Ball passes paddle on left - Player B scored, ball changes to their color, point added, speed adjusted
     if ball.xcor() < -395:
         ball.setx(0)
         ball.dx *= -1
         score_b +=1
         wn.bgcolor("orange")
+        ball.shape("circle")
         ball.color("orange")
         pen.clear()
+        ball.shapesize(stretch_wid=1, stretch_len=1)
         #Reset ball speed if Player B scores
-        ball.dx = 0.11
-        ball.dy = 0.12
+        ball.dx = 0.19
+        ball.dy = 0.16
         pen.write("Player A: {}  Player B: {}".format(score_a, score_b), align="center", font=("courier", 24, "normal"))
         time.sleep(.1)
         wn.bgcolor("black")
@@ -146,12 +183,18 @@ while True:
         ball.dy = 0.12
         #Finish game after X rounds and output Player B as winner
         if score_b > 2:
+            print(time.time())
+            time.sleep(.14)
             pen.clear()
             wn.clear()
             wn.bgcolor("orange")
             pen.goto(0, 160)
-            pen.write("Player B Wins with {} points!" .format(score_b), align="center", font=("lato", 32, "normal"))
-            print("Player B Wins with {} points!" .format(score_b))
+            pen.write("Player B Wins with:" .format(score_b, b_collisions), align="center", font=("lato", 32, "normal"))
+            pen.goto(0, 100)
+            pen.write("{} points" .format(score_b), align="center", font=("lato", 32, "normal"))
+            pen.goto(0, 40)
+            pen.write("You hit the ball {} time(s) " .format(b_collisions), align="center", font=("lato", 32, "normal"))
+            print("Player B Wins with {} points and {} collisions" .format(score_b, b_collisions))
             time.sleep(3)
             break
 
@@ -161,6 +204,44 @@ while True:
         ball.dx *= -1
         ball.dx *= 1.14
         ball.sety(ball.ycor() + ball.dy + 5)
+        a_collisions += 1
+        #Random "Super attack"
+        rand = random.randrange(0, 15) # 1/15 chance - Update both player's rand variables for fair gameplay. (or don't)
+        if rand < 2:
+            wn.bgcolor("white")
+            time.sleep(.1)
+            wn.bgcolor("red")
+            time.sleep(.1)
+            wn.bgcolor("white")
+            time.sleep(.1)
+            ball.dy = 0.12
+            ball.dx = 0.11
+            ball.dy *= 6.1
+            ball.dx *= 9.2
+            wn.bgcolor("black")
+            ball.color("red")
+        #Nuclear Super attack
+        if keyboard.is_pressed('Space'):
+            ball.dy = 1.9
+            ball.dx = 1.3
+            ball.shape("triangle")
+            ball.shapesize(stretch_wid=4, stretch_len=4)
+            wn.bgcolor("white")
+            time.sleep(.1)
+            wn.bgcolor("red")
+            time.sleep(.1)
+            wn.bgcolor("white")
+            time.sleep(.2)
+            wn.bgcolor("red")
+            time.sleep(.2)
+            wn.bgcolor("black")
+            time.sleep(.1)
+        #When ball collides with paddle, ball's y axis is random
+        rand = random.randrange(0, 2)
+        if rand < 2:
+            ball.dy = -.11
+        if rand < 1:
+            ball.dy = .11
 
        #Player B
     if (ball.xcor() > 330 and ball.xcor() < 340) and (ball.ycor() < paddle_b.ycor() + 55 and ball.ycor() > paddle_b.ycor() -55):
@@ -168,6 +249,45 @@ while True:
         ball.dx *= -1
         ball.dx *= 1.14
         ball.sety(ball.ycor() + ball.dy + 5)
+        b_collisions += 1
+        #Auto Super attack
+        rand = random.randrange(0, 15) # 1/15 chance - Update both player's rand variables for fair gameplay. (or don't)
+        if rand < 2:
+            wn.bgcolor("white")
+            time.sleep(.1)
+            wn.bgcolor("red")
+            time.sleep(.1)
+            wn.bgcolor("white")
+            time.sleep(.1)
+            ball.dy = 0.12
+            ball.dx = -0.11
+            ball.dy *= 6.1
+            ball.dx *= 9.2
+            wn.bgcolor("black")
+            ball.color("red")
+        #Nuclear Super attack
+        if keyboard.is_pressed('0'):
+            ball.shape("triangle")
+            ball.dy = 1.9
+            ball.dx = -1.3
+            ball.shapesize(stretch_wid=4, stretch_len=4)
+            wn.bgcolor("white")
+            time.sleep(.1)
+            wn.bgcolor("orange")
+            time.sleep(.1)
+            wn.bgcolor("yellow")
+            time.sleep(.1)
+            wn.bgcolor("white")
+            time.sleep(.2)
+            wn.bgcolor("black")
+            time.sleep(.1)
+
+        #When ball collides with paddle, ball's y axis is random
+        rand = random.randrange(0, 3)
+        if rand < 2:
+            ball.dy = -.11
+        if rand < 2:
+            ball.dy = .11
 
     #Stop paddles from going off screen
        #Player A
